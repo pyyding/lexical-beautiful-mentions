f253320ec7dd6cef3a29abbaf4cdfcf006e99e56
pyyding <kaspar.pyyding@gmail.com>
2023-11-15 20:44:12 +0200

    mention label build

--- plugin/publish-lib.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-const { execSync } = require("child_process");
-
-const libPath = path.join(__dirname, "./lib");
-
-execSync("npm publish", { cwd: libPath, stdio: "inherit" });
